4da470ecde2142b8b87dedda31cc5060f8fed193
Moses <91290515+mosesluna@users.noreply.github.com>
2021-11-20 19:15:26 -0500

    This is what we have for phase 3, very rough work.

---
+++ Group Project/TicTacToe.js
@@ -0,0 +1,8 @@
+function userInput() {
+  var userName = prompt("What is your name?", "");
+  var player = prompt(
+    "Enter '1' to verse bot. Enter '2' to verse another player.",
+    ""
+  );
+  var place = prompt("Enter '1' to go first. Enter '2' to go second.", "");
+}
